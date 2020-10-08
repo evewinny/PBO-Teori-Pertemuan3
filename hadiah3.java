fbe2bed2c427c31e85bbd4c66b16816f3ebbc592
Evelyn W <71800800+evewinny@users.noreply.github.com>
2020-10-08 12:56:35 +0800

    Add files via upload

---
+++ hadiah3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class hadiah3 {
+	public static void main(String[] args) {
+		Scanner masukan = new Scanner(System.in);	
+	System.out.println("Nomor 1");
+	int aa = 5;
+	int bb = 6;
+	double cc = 0.5;
+	System.out.println("aa=" + aa+ "bb =" + bb+ "cc =" + cc);
+	System.out.println("aa + bb = " + (aa+bb));
+	System.out.println("aa x bb x cc =" + (aa*bb*cc));
+	System.out.println("bb / cc =" + (bb/cc));
+	System.out.println();
+	
+	System.out.println("Nomor 2");
+	String nama, alamat;
+	String kota = "Makassar";
+	int umur;
+	int saudara = 2;
+	
+	System.out.print("Masukkan nama mu = ");
+	nama = masukan.nextLine();
+	System.out.print("Masukkan alamat mu = ");
+	alamat = masukan.nextLine();
+	System.out.print("Masukkan umur mu = ");
+	umur = masukan.nextInt();
+		System.out.println("Saudara =" + saudara);
+		System.out.println("Tinggal di " +kota);
+		System.out.println("Berumur "+umur+" tahun");
+	}
+}
